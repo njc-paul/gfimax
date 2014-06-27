0afcc635f15339a4b61d9d8607b11a59fac5d9a5
njc-paul <paul@notjustcomputers.co.uk>
2014-06-27 21:36:31 +0100

    Create ChangePowerSettings.bat
    When setting up the automated task in the command line box, enter 60 for a 1 hour power down or 0 to disable power save

---
+++ ChangePowerSettings.bat
@@ -0,0 +1,19 @@
+@echo off
+:parse
+IF "%~1"=="0" GOTO nosleep
+IF not "%~1"=="" GOTO setsleep
+SHIFT
+GOTO parse
+ 
+:setsleep
+echo Sleep mode Activated for %~1 Mins
+powercfg.exe -change -standby-timeout-ac %~1
+GOTO end
+ 
+ 
+:nosleep
+echo Sleep mode deactivated
+powercfg.exe -change -standby-timeout-ac 0
+GOTO end
+ 
+:end
